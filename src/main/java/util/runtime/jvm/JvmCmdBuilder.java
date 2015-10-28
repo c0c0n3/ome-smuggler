4f49da78e0382060d0a9ea01b4cebe395af7cfa7
andrea falconi <andrea.falconi@gmail.com>
2015-10-28 21:48:10 +0100

    flesh out util jvm cmd builder

--- src/main/java/util/runtime/jvm/JvmCmdBuilder.java
+++ src/main/java/util/runtime/jvm/JvmCmdBuilder.java
@@ -1,7 +1,11 @@
 package util.runtime.jvm;
 
 import static java.util.Objects.requireNonNull;
+import static java.util.function.Function.identity;
+import static util.sequence.Arrayz.asStream;
+import static util.sequence.Arrayz.hasNulls;
 
+import java.nio.file.Path;
 import java.util.stream.Stream;
 
 /**
@@ -9,18 +13,33 @@ import java.util.stream.Stream;
  */
 public class JvmCmdBuilder {
 
-    public static Stream<String> java(JarJvmArg appToRun, 
-                                      Stream<SysPropJvmArg> props,
-                                      JvmArgument<?>...appArgs) {
-        requireNonNull(appToRun, "appToRun");
-        requireNonNull(props, "props");
-        
-        return null;
+    private static Stream<String> build(Stream<Stream<Object>> xs) {
+        return xs.flatMap(identity())
+                 .map(x -> (JvmArgument<?>) x)
+                 .map(JvmArgument::tokens)
+                 .flatMap(identity());
     }
     
-    public static Stream<String> java(JarJvmArg appToRun, 
+    public static Stream<String> java(JvmArgument<Path> jrePath,
+                                      JarJvmArg appToRun, 
+                                      Stream<SysPropJvmArg> props,
                                       JvmArgument<?>...appArgs) {
-        return java(appToRun, Stream.empty(), appArgs);
+        requireNonNull(jrePath, "jrePath");
+        requireNonNull(appToRun, "appToRun");
+        requireNonNull(props, "props");
+        if (hasNulls(appArgs)) {  // false if appArgs is null or zero len
+            throw new NullPointerException("appArgs has null elements");
+        }
+        
+        Stream<Stream<Object>> xs = Stream.of(Stream.of(jrePath), 
+                Stream.of(appToRun), Stream.of(props), asStream(appArgs));
+        return build(xs);
+    }
+    
+    public static Stream<String> java(JvmArgument<Path> jrePath,
+                                      JarJvmArg appToRun, 
+                                      JvmArgument<?>...appArgs) {
+        return java(jrePath, appToRun, Stream.empty(), appArgs);
     }
   
 }
