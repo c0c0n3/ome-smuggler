a68a19a2caa4d6aa37edb8ce4af3225cd67e81bb
andrea falconi <andrea.falconi@gmail.com>
2015-11-18 01:02:02 +0100

    factor formatting code out of import output into own class

--- src/main/java/ome/smuggler/core/service/impl/ImportOutput.java
+++ src/main/java/ome/smuggler/core/service/impl/ImportOutput.java
@@ -2,8 +2,8 @@ package ome.smuggler.core.service.impl;
 
 import static java.nio.file.StandardOpenOption.*;
 import static java.util.Objects.requireNonNull;
+import static ome.smuggler.core.service.impl.ImportOutputFormatter.*;
 import static util.error.Exceptions.throwAsIfUnchecked;
-import static util.string.Strings.write;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -13,9 +13,9 @@ import java.util.Arrays;
 import java.util.List;
 
 import ome.smuggler.config.items.ImportLogConfig;
-import ome.smuggler.core.types.ImportInput;
 import ome.smuggler.core.types.ImportLogPath;
 import ome.smuggler.core.types.QueuedImport;
+
 
 public class ImportOutput {
 
@@ -41,54 +41,6 @@ public class ImportOutput {
         return new ImportLogPath(dir, task.getTaskId()).get();
     }
     
-    private String queued() {
-        return write(out -> {
-            ImportInput req = task.getRequest();
-            out.println("====================> Queued OMERO Import:");
-            out.format("Import ID: %s%n", task.getTaskId());
-            out.format("Import Target: %s%n", req.getTarget());
-            out.format("OMERO Server: %s%n", req.getOmeroHost());
-            out.format("Experimenter's Email: %s%n", req.getExperimenterEmail());
-            out.println("============================================");
-        });
-    }
-    
-    private String header(ImporterCommandBuilder importCommand) {
-        return write(out -> {
-            ImportInput req = task.getRequest();
-            out.println("====================> Starting OMERO Import:");
-            out.format("Import ID: %s%n", task.getTaskId());
-            out.format("Import Target: %s%n", req.getTarget());
-            out.format("OMERO Server: %s%n", req.getOmeroHost());
-            out.format("Experimenter's Email: %s%n", req.getExperimenterEmail());
-            out.format("Import Command: %s%n", importCommand);
-            out.format("Output File: %s%n", outputFile);
-            out.println("============================================");
-        });
-    }
-    
-    private String footer(boolean success, int exitStatus) {
-        return write(out -> {
-            out.println();
-            out.println("=================> OMERO Import Run Report:");
-            out.format("Succeeded: %s%n", success);
-            out.format("Exit Status: %s%n", exitStatus);
-            out.println("============================================");
-        });
-    }
-    
-    private String footer(Exception e) {
-        return write(out -> {
-            out.println();
-            out.println("=================> OMERO Import Run Report:");
-            out.format("Succeeded: %s%n", false);
-            out.format("Cause of Failure: %s%n", e.getMessage());
-            out.println("Error Detail:");
-            e.printStackTrace(out);
-            out.println("============================================");
-        });
-    }
-    
     private void output(String line) throws IOException {
         ensureDirectories(outputFile);
         List<String> content = Arrays.asList(line);
@@ -100,13 +52,13 @@ public class ImportOutput {
     }
     
     public void writeQueued() throws IOException {
-        output(queued());
+        output(queued(task));
     }
     
     public void writeHeader(ImporterCommandBuilder importCommand) 
             throws IOException {
         Files.delete(outputFile);
-        output(header(importCommand));
+        output(header(task, importCommand, outputFile));
     }
     
     public void writeFooter(boolean success, int exitStatus) throws IOException {
---
+++ src/main/java/ome/smuggler/core/service/impl/ImportOutputFormatter.java
@@ -0,0 +1,99 @@
+package ome.smuggler.core.service.impl;
+
+import static util.string.Strings.write;
+
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import ome.smuggler.core.types.ImportInput;
+import ome.smuggler.core.types.QueuedImport;
+
+public class ImportOutputFormatter {
+
+    private static void field(PrintWriter out, String name, Object value) {
+        out.format("%s: %s%n", name, value);
+    }
+    
+    private static void timestamp(PrintWriter out) {
+        Date now = Calendar.getInstance().getTime();
+        String value = DateFormat.getDateTimeInstance().format(now);
+        field(out, "Timestamp", value);
+    }
+    
+    private static void importId(PrintWriter out, QueuedImport task) {
+        field(out, "Import ID", task.getTaskId());
+    }
+    
+    private static void importTarget(PrintWriter out, ImportInput req) {
+        field(out, "Import Target", req.getTarget());
+    }
+    
+    private static void omeroServer(PrintWriter out, ImportInput req) {
+        field(out, "OMERO Server", req.getOmeroHost());
+    }
+    
+    private static void experimenterEmail(PrintWriter out, ImportInput req) {
+        field(out, "Experimenter's Email", req.getExperimenterEmail());
+    }
+    
+    private static void summary(PrintWriter out, QueuedImport task) {
+        ImportInput req = task.getRequest();
+        
+        timestamp(out);
+        importId(out, task);
+        importTarget(out, req);
+        omeroServer(out, req);
+        experimenterEmail(out, req);
+    }
+    
+    private static void succeeded(PrintWriter out, boolean success) {
+        timestamp(out);
+        field(out, "Succeeded", success);
+    }
+    
+    public static String queued(QueuedImport task) {
+        return write(out -> {    
+            out.println("====================> Queued OMERO Import:");
+            summary(out, task);
+            out.println("============================================");
+        });
+    }
+    
+    public static String header(QueuedImport task,
+                                ImporterCommandBuilder importCommand,
+                                Path outputFile) {
+        return write(out -> {
+            out.println("====================> Starting OMERO Import:");
+            summary(out, task);
+            field(out, "Import Command", importCommand);
+            field(out, "Output File", outputFile);
+            out.println("============================================");
+        });
+    }
+    
+    public static String footer(boolean success, int exitStatus) {
+        return write(out -> {
+            out.println();
+            out.println("=================> OMERO Import Run Report:");
+            succeeded(out, success);
+            field(out, "Exit Status", exitStatus);
+            out.println("============================================");
+        });
+    }
+    
+    public static String footer(Exception e) {
+        return write(out -> {
+            out.println();
+            out.println("=================> OMERO Import Run Report:");
+            succeeded(out, false);
+            field(out, "Cause of Failure", e.getMessage());
+            out.println("Error Detail:");
+            e.printStackTrace(out);
+            out.println("============================================");
+        });
+    }
+    
+}
