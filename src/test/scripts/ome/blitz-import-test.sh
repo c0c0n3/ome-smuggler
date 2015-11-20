2aa9f335c1940c282f7211e3c1548bfe94fc35eb
Andrea Falconi <andrea.falconi@gmail.com>
2015-11-20 12:56:41 +0100

    add tmp test scripts, will prob get rid of them later

---
+++ src/test/scripts/ome/blitz-import-test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source openmicroscopy/components/blitz/target/generated/resources/Classpath.sh
+
+java -cp $CLASSPATH ome.formats.importer.cli.CommandLineImporter "$@"
---
+++ src/test/scripts/ome/show-blitz-cp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source openmicroscopy/components/blitz/target/generated/resources/Classpath.sh
+# same as openmicroscopy/components/blitz/target/classes/Classpath.sh
+# (verified)
+
+echo $CLASSPATH | awk -F':' '{ for (i = 0; ++i <= NF;) print $i }'
