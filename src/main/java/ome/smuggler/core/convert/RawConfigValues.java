1dc0928b173bba67abf053f7eba0ebbeb81a6d8b
Andrea Falconi <andrea.falconi@gmail.com>
2016-03-08 17:17:43 +0100

    move raw config value converters in own class

---
+++ src/main/java/ome/smuggler/core/convert/RawConfigValues.java
@@ -0,0 +1,50 @@
+package ome.smuggler.core.convert;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
+import static util.string.Strings.requireString;
+
+import java.net.URI;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+import ome.smuggler.core.types.Nat;
+import ome.smuggler.core.types.PositiveN;
+
+/**
+ * Utility methods to convert raw config values to values of more suitable 
+ * types.
+ */
+public class RawConfigValues {
+    
+    public static Path toPath(String path) {
+        requireString(path, "path");
+        return Paths.get(path);
+    }
+    
+    public static Duration toDuration(Long minutes) {
+        PositiveN v = PositiveN.of(minutes);  // NB throws if minutes <= 0
+        return Duration.ofMinutes(v.get());
+    }
+    
+    public static List<Duration> toDurationList(Long[] minutes) {
+        return Stream.of(minutes == null ? new Long[0] : minutes)
+                     .map(ms -> toDuration(ms))
+                     .collect(collectingAndThen(
+                                 toList(), Collections::unmodifiableList));
+    }
+    
+    public static URI toURI(String scheme, String host, int port) {
+        requireString(scheme, "scheme");
+        requireString(host, "host");
+        Nat p = Nat.of(port);  // NB throws if port <= 0
+        
+        String raw = String.format("%s://%s:%s", scheme, host, p);
+        return URI.create(raw);  // NB throws if not valid
+    }
+    
+}
--- src/main/java/ome/smuggler/core/types/ImportConfigReader.java
+++ src/main/java/ome/smuggler/core/types/ImportConfigReader.java
@@ -1,16 +1,13 @@
 package ome.smuggler.core.types;
 
 import static java.util.Objects.requireNonNull;
-import static java.util.stream.Collectors.collectingAndThen;
-import static java.util.stream.Collectors.toList;
-import static util.string.Strings.requireString;
+import static ome.smuggler.core.convert.RawConfigValues.toDuration;
+import static ome.smuggler.core.convert.RawConfigValues.toDurationList;
+import static ome.smuggler.core.convert.RawConfigValues.toPath;
 
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.time.Duration;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Stream;
 
 import ome.smuggler.config.items.ImportConfig;
 
@@ -19,23 +16,6 @@ import ome.smuggler.config.items.ImportConfig;
  * from an underlying {@link ImportConfig}.
  */
 public class ImportConfigReader implements ImportConfigSource {
-
-    private static Path toPath(String path) {
-        requireString(path, "path");
-        return Paths.get(path);
-    }
-    
-    private static Duration toDuration(Long minutes) {
-        PositiveN v = PositiveN.of(minutes);  // NB throws if minutes <= 0
-        return Duration.ofMinutes(v.get());
-    }
-    
-    private static List<Duration> toDurationList(Long[] minutes) {
-        return Stream.of(minutes == null ? new Long[0] : minutes)
-                     .map(ms -> toDuration(ms))
-                     .collect(collectingAndThen(
-                                 toList(), Collections::unmodifiableList));
-    }
     
     private final Path importLogDir;
     private final Duration logRetentionPeriod;
