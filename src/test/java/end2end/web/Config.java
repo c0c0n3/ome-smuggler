6f21b51e75eb2fe514b8db8743d16b3685ed9793
Andrea Falconi <andrea.falconi@gmail.com>
2016-03-18 12:06:31 +0100

    add mail config and failed mail store to end 2 end test config

--- src/test/java/end2end/web/Config.java
+++ src/test/java/end2end/web/Config.java
@@ -5,18 +5,27 @@ import java.nio.file.Path;
 
 import ome.smuggler.config.BaseDataDir;
 import ome.smuggler.config.data.DevImportConfigSource;
+import ome.smuggler.config.data.DevMailConfigSource;
+import ome.smuggler.config.wiring.mail.MailServiceBeans;
+import ome.smuggler.core.service.file.TaskFileStore;
 import ome.smuggler.core.types.ImportConfigSource;
+import ome.smuggler.core.types.MailConfigSource;
+import ome.smuggler.core.types.MailId;
 
 public class Config {
     
     public final Path baseDataDir;
     public final ImportConfigSource importConfig;
+    public final MailConfigSource mailConfig;
+    public final TaskFileStore<MailId> failedMailStore;
     
     public Config() throws IOException {
         baseDataDir = BaseDataDir.setupSysPropToTempDir("smuggler-tests").get();
         
         importConfig = new DevImportConfigSource(baseDataDir);
-        // ...an exact copy of the one used by Spring.
+        mailConfig = new DevMailConfigSource(baseDataDir);
+        failedMailStore = new MailServiceBeans().failedMailStore(mailConfig);
+        // ...exact copies of the ones instantiated in the Spring container.
     }
     
 }
