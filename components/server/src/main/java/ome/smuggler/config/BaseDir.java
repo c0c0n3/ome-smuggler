2b7ec0693fb6f05cab2d94ee803c766645e45bef
andrea falconi <andrea.falconi@gmail.com>
2016-04-16 21:21:04 +0200

    introduce class to factor out functionality common to base directories used in config.

---
+++ components/server/src/main/java/ome/smuggler/config/BaseDir.java
@@ -0,0 +1,103 @@
+package ome.smuggler.config;
+
+import static java.util.Objects.requireNonNull;
+import static util.string.Strings.asOptional;
+import static util.string.Strings.isNullOrEmpty;
+import static util.string.Strings.requireString;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import util.object.Wrapper;
+
+/**
+ * Path to a directory used as a base for {@link #resolve(String) resolving} 
+ * paths in configuration files.
+ * The base directory path is retrieved from the system properties if a property
+ * value for a specified key exists; otherwise it will be the current working
+ * directory, i.e. the value of "user.dir".
+ */
+public class BaseDir extends Wrapper<Path> {
+
+    /**
+     * Looks up the path associated to the specified key in the system 
+     * properties. If no value is found for the key, the current working
+     * directory is returned.
+     * @param key the system property key.
+     * @return the path.
+     * @throws IllegalArgumentException if the argument is {@code null} or
+     * empty.
+     */
+    public static Path lookup(String key) {
+        requireString(key, "key");
+        
+        String dir = asOptional(System.getProperty(key))
+                    .orElse(System.getProperty("user.dir"));
+        return Paths.get(dir);
+    }
+    
+    /**
+     * Sets a system property with the specified key and value.
+     * @param key the key.
+     * @param dir the value.
+     * @throws IllegalArgumentException if the key is {@code null} or empty.
+     * @throws NullPointerException if {@code dir} is {@code null}.
+     */
+    public static void store(String key, Path dir) {
+        requireString(key, "key");
+        requireNonNull(dir, "dir");
+        
+        System.setProperty(key, dir.toString());
+    }
+    
+    /**
+     * Creates a temporary directory and sets its path in the system properties 
+     * using the given key. 
+     * @param key the system property key.
+     * @param dirNamePrefix name prefix of the temporary directory to create.
+     * @return the path to the newly created temporary directory.
+     * @throws IOException if the directory could not be created.
+     * @throws IllegalArgumentException if any argument is {@code null} or 
+     * empty.
+     */
+    public static Path storeTempDir(String key, String dirNamePrefix) 
+            throws IOException {
+        requireString(key, "key");
+        requireString(dirNamePrefix, "dirNamePrefix");
+        
+        Path dir = Files.createTempDirectory(dirNamePrefix);
+        System.setProperty(key, dir.toString());
+        return dir;
+    }
+
+    /**
+     * Creates a new instance from the system property having the specified key.
+     * If no such a property exists, then the current working directory is
+     * used instead.
+     * @param sysPropKey the system property key.
+     * @throws IllegalArgumentException if the key is {@code null} or empty.
+     */
+    public BaseDir(String sysPropKey) {
+        super(lookup(sysPropKey));
+    }
+
+    /**
+     * Resolves the given path as follows.
+     * If the argument is already an absolute path, then it is returned as is;
+     * otherwise it is resolved against this base directory. In particular if
+     * the path argument is {@code null} or empty, this base directory is
+     * returned.
+     * @param configuredPath a path in a configuration file.
+     * @return the resolved path.
+     */
+    public Path resolve(String configuredPath) {
+        if (isNullOrEmpty(configuredPath)) {
+            return get();
+        } else {
+            return get().resolve(configuredPath);
+        }
+    }
+
+}
---
+++ components/server/src/test/java/ome/smuggler/config/BaseDirTest.java
@@ -0,0 +1,71 @@
+package ome.smuggler.config;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.Test;
+
+public class BaseDirTest {
+
+    private static final String key = "non-existing";
+    
+    private static BaseDir newBaseDir() {
+        return new BaseDir(key);
+    }
+    
+    private static void setProp(Path dir) {
+        BaseDir.store(key, dir);
+    }
+    
+    private static void unsetProp() {
+        System.setProperty(key, "");
+    }
+    
+    private static Path pwd(String...ps) {
+        return Paths.get(System.getProperty("user.dir"), ps);
+    }
+    
+    @Test
+    public void defaultToPwd() {
+        assertThat(newBaseDir(), is(pwd()));
+    }
+
+    @Test
+    public void useAsIsIfSet() {
+        Path dir = Paths.get("rel", "path");
+        setProp(dir);
+        
+        assertThat(newBaseDir(), is(dir));
+        
+        unsetProp();
+    }
+
+    @Test
+    public void resolveNullConfigPathToBase() {
+        Path actual = newBaseDir().resolve(null);
+        assertThat(actual, is(pwd()));
+    }
+
+    @Test
+    public void resolveEmptyConfigPathToBase() {
+        Path actual = newBaseDir().resolve("");
+        assertThat(actual, is(pwd()));
+    }
+    
+    @Test
+    public void resolveAbsoluteConfigPathToSelf() {
+        Path absPath = pwd("x").toAbsolutePath();
+        Path actual = newBaseDir().resolve(absPath.toString());
+        assertThat(actual, is(absPath));
+    }
+    
+    @Test
+    public void resolveRelativeConfigPathAgainstBaseDir() {
+        Path actual = newBaseDir().resolve("x");
+        assertThat(actual, is(pwd("x")));
+    }
+    
+}
