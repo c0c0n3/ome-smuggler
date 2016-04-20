8f4f1c9f5735f65715b79631db32644e13ed69d9
Andrea Falconi <andrea.falconi@gmail.com>
2016-04-20 19:28:15 +0200

    implement omero service env.

---
+++ components/server/src/main/java/ome/smuggler/core/service/omero/impl/OmeroEnv.java
@@ -0,0 +1,44 @@
+package ome.smuggler.core.service.omero.impl;
+
+import static java.util.Objects.requireNonNull;
+
+import ome.smuggler.core.types.OmeCliConfigSource;
+
+/**
+ * Provides access to service configuration and auxiliary services such as
+ * logging.
+ */
+public class OmeroEnv {
+
+    private final OmeCliConfigSource config;
+    private final OmeroLogger log;
+
+    /**
+     * Creates a new instance.
+     * @param config provides access the the configuration values.
+     * @param log the log service.
+     * @throws NullPointerException if any argument is {@code null}.
+     */
+    public OmeroEnv(OmeCliConfigSource config, OmeroLogger log) {
+        requireNonNull(config, "config");
+        requireNonNull(log, "log");
+
+        this.config = config;
+        this.log = log;
+    }
+
+    /**
+     * @return the configuration values.
+     */
+    public OmeCliConfigSource config() {
+        return config;
+    }
+
+    /**
+     * @return the log service.
+     */
+    public OmeroLogger log() {
+        return log;
+    }
+
+}
---
+++ components/server/src/main/java/ome/smuggler/core/service/omero/impl/OmeroLogger.java
@@ -0,0 +1,15 @@
+package ome.smuggler.core.service.omero.impl;
+
+import ome.smuggler.core.service.log.BaseLogger;
+import ome.smuggler.core.service.log.LogService;
+
+/**
+ * Logging methods for the OMERO services.
+ */
+public class OmeroLogger extends BaseLogger {
+
+    public OmeroLogger(LogService service) {
+        super(service);
+    }
+
+}
