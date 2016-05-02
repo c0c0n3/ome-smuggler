231405548e44fe4237f565d7c439ec043227a531
andrea falconi <andrea.falconi@gmail.com>
2016-05-02 13:02:51 +0200

    use gradle's buildSrc mechanism to factor out file utils.

---
+++ buildSrc/src/main/groovy/util/Filez.groovy
@@ -0,0 +1,39 @@
+package util
+
+import java.nio.file.Paths
+import org.gradle.api.tasks.bundling.Jar
+
+/**
+ * Utility methods to operate on file paths as Gradle likes File better than
+ * Path.
+ */
+class Filez {
+
+    /**
+     * Combines the base path with the rest of the path elements.
+     * Examples:
+     * <pre>
+     *     File base = new File('mydir')
+     *     toFile(base)                       ~~== mydir
+     *     toFile(base, 'some')               ~~== mydir/some
+     *     toFile(base, 'some/other')         ~~== mydir/some/other
+     *     toFile(base, 'some/other', 'path') ~~== mydir/some/other/path
+     * </pre>
+     * @param base the base path.
+     * @param ps the rest of the path elements.
+     * @return the combined path as a file.
+     */
+    static File toFile(File base, String...ps) {
+        Paths.get(base.absolutePath, ps).toFile()
+    }
+
+    /**
+     * Converts the Jar task's archive path into a file.
+     * @param task the task.
+     * @return the file.
+     */
+    static File toFile(Jar task) {
+        toFile(task.archivePath)
+    }
+
+}
