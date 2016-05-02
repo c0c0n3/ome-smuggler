e3f2394c10582814c9b1cd80565586338e6ae0ba
Andrea Falconi <andrea.falconi@gmail.com>
2016-05-02 15:09:17 +0200

    introduce util replacement map for build scripts.

---
+++ buildSrc/src/main/groovy/util/ReplacementMap.groovy
@@ -0,0 +1,21 @@
+package util
+
+/**
+ * A map with the ability to replace key occurrences with corresponding values
+ * in a given string.
+ */
+class ReplacementMap<K, V> extends HashMap<K, V> {
+
+    /**
+     * For each key {@code k} in this map, replaces any occurrence of {@code k}
+     * in the input string with the value associated to {@code k} in this map.
+     * @param x the input string to process.
+     * @return a new string with the replaced values.
+     */
+    String replaceAll(String x) {
+        inject(x, { r, e ->
+            r.replaceAll(e.key.toString(), e.value.toString())
+        })
+    }
+
+}
